fe5efeed676c426f7ea2fc4fa49c2f06377f47ba
yws8520 <93501925+yws8520@users.noreply.github.com>
2026-09-24 11:57:34 +0800

    Update check_beyblade.py

--- check_beyblade.py
+++ check_beyblade.py
@@ -8,8 +8,8 @@ from playwright.sync_api import sync_playwright
 # List of target keywords to search for
 TARGET_KEYWORDS = ["爆旋陀螺", "BX-52", "BX-53"]
 
+# "https://www.toysrus.com.hk/zh-hk/search/?q=%28%E7%B6%B2%E5%BA%97%E9%A0%90%E8%B3%BC%29+Beyblade+X&lang=zh_HK&cgid=",
 URLS = [
-    "https://www.toysrus.com.hk/zh-hk/whats-on/new-arrivals/pre-order/",
     "https://www.hobbylandeshop.com/product-category/nproduct_booking",
 ]
 
