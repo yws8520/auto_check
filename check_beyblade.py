5228ac04ff6f51fcf57a0b31869f45e730757541
yws8520 <93501925+yws8520@users.noreply.github.com>
2026-09-25 10:27:48 +0800

    Add new preorder link for Beyblade

--- check_beyblade.py
+++ check_beyblade.py
@@ -16,6 +16,7 @@ URLS = [
     "https://www.hobbylandeshop.com/product-category/takaratomy/beyblade%E9%99%80%E8%9E%BA",
     "https://fooklemodel.com/product-tag/pre-order/?_stockstatus=1",
     "https://lastchancetoy.com/search?q=BeybladeX",
+    "https://www.aeoncity.com.hk/preorder_zh/",
 ]
 
 # Credentials
